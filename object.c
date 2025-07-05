cbb712e697ed887984870b2d0e08441eb5fc41d1
divolli <dpavlysh@gmail.com>
2025-07-05 21:48:02 +0200

    Added code

--- object.c
+++ object.c
@@ -35,3 +35,74 @@ snake_obj_t *new_snake_string(char *string){
   new->kind = STRING;
   return new;
 }
+
+
+snake_obj_t *new_snake_vector(snake_obj_t *x, snake_obj_t *y, snake_obj_t *z){
+  if (!x || !y || !z) return NULL;
+  snake_obj_t *new = (snake_obj_t *) malloc(sizeof(snake_obj_t));
+  if (!new) return NULL;
+
+  new->kind = VECTOR;
+  new->data.v_vector.x = x;
+  new->data.v_vector.y = y;
+  new->data.v_vector.z = z;
+
+  return new;
+}
+
+
+snake_obj_t *new_snake_array(size_t size){
+  if (size < 1) return NULL;
+
+  snake_obj_t *new = (snake_obj_t *)malloc(sizeof(snake_obj_t));
+  if (!new) return NULL;
+
+  new->kind = ARRAY;
+  new->data.v_array.size = size;
+  new->data.v_array.elements = (snake_obj_t **) calloc(size, sizeof(snake_obj_t *));
+  if (!new->data.v_array.elements){
+    free(new);
+    return NULL;
+  }
+
+  return new;
+}
+
+
+int snake_array_set(snake_obj_t *snake_obj, size_t index, snake_obj_t *value){
+  // reutrns 0 if false and 1 if true
+  if (!snake_obj || !value) return 0;
+  if (snake_obj->kind != ARRAY) return 0;
+  if (snake_obj->data.v_array.size <= index) return 0;
+
+  *(snake_obj->data.v_array.elements + index) = value;
+  return 1;
+}
+
+
+snake_obj_t *snake_array_get(snake_obj_t *snake_obj, size_t index){
+  if (!snake_obj || snake_obj->kind != ARRAY) return NULL;
+  if (snake_obj->data.v_array.size <= index) return NULL;
+
+  return *(snake_obj->data.v_array.elements + index);
+}
+
+
+int snake_length(snake_obj_t *obj){
+  if (!obj) return -1;
+
+  switch(obj->kind){
+    case INTEGER : return 1;
+    case FLOAT : return 1;
+    case STRING : return strlen(obj->data.v_string);
+    case VECTOR : return 3;
+    case ARRAY : return obj->data.v_array.size;
+    default : return -1;
+  }
+
+}
+
+
+snake_obj_t *snake_add(snake_obj_t *a, snake_obj_t *b){
+
+}
--- object.h
+++ object.h
@@ -1,6 +1,7 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <stddef.h>
 typedef struct snake_obj snake_obj_t ;
 typedef struct snake_vector snake_vector_t;
 
@@ -8,22 +9,15 @@ typedef enum snake_obj_kind {
   INTEGER,
   FLOAT,
   STRING,
-  VECTOR
+  VECTOR,
+  ARRAY
 }snake_obj_kind_t;
 
 
-typedef union snake_obj_data {
-  int v_int;
-  float v_float;
-  char *v_string;
-  snake_vector_t v_vector;
-}snake_obj_data_t;
-
-
-typedef struct snake_obj {
-  snake_obj_kind_t kind;
-  snake_obj_data_t data;
-}snake_obj_t;
+typedef struct  snake_array {
+  size_t size;
+  snake_obj_t **elements;
+}snake_array_t;
 
 
 typedef struct snake_vector{
@@ -33,9 +27,31 @@ typedef struct snake_vector{
 }snake_vector_t;
 
 
+typedef union snake_obj_data {
+  int v_int;
+  float v_float;
+  char *v_string;
+  snake_vector_t v_vector;
+  snake_array_t v_array;
+}snake_obj_data_t;
+
+
+
+typedef struct snake_obj {
+  snake_obj_kind_t kind;
+  snake_obj_data_t data;
+}snake_obj_t;
+
+
 snake_obj_t *new_snake_int(int value);
 snake_obj_t *new_snake_float(float value);
 snake_obj_t *new_snake_string(char *string);
-snake_obj_t *new_snake_vector();
+snake_obj_t *new_snake_vector(snake_obj_t *x, snake_obj_t *y, snake_obj_t *z);
+snake_obj_t *new_snake_array(size_t size);
+snake_obj_t *snake_array_get(snake_obj_t *snake_obj, size_t index);
+snake_obj_t *snake_add(snake_obj_t *a, snake_obj_t *b);
+int snake_array_set(snake_obj_t *snake_obj, size_t index, snake_obj_t *value);
+int snake_length(snake_obj_t *obj);
+
 
 #endif // OBJECT_H
