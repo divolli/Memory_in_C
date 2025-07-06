56b140d220ba826f367ea10874b87ed6851d2fd7
divolli <dpavlysh@gmail.com>
2025-07-06 16:18:01 +0200

    Added code

--- object.c
+++ object.c
@@ -1,7 +1,7 @@
 #include "object.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 
 snake_obj_t *new_snake_int(int value){
   snake_obj_t *new = (snake_obj_t *) malloc(sizeof(snake_obj_t));
@@ -104,5 +104,76 @@ int snake_length(snake_obj_t *obj){
 
 
 snake_obj_t *snake_add(snake_obj_t *a, snake_obj_t *b){
+  if (!a || !b) return NULL;
 
+  switch (a->kind){
+    case INTEGER:
+      switch (b->kind){
+        case INTEGER: return new_snake_int(a->data.v_int + b->data.v_int);
+        case FLOAT : return new_snake_float((float)a->data.v_int + b->data.v_float);
+        default:
+          printf("Invalid operation\n");
+          return NULL;
+      }
+    case FLOAT :
+      switch (b->kind){
+        case INTEGER: return new_snake_float(a->data.v_float + (float)b->data.v_int);
+        case FLOAT : return new_snake_float(a->data.v_float + b->data.v_float);
+        default:
+          printf("Invalid operation\n");
+          return NULL;
+      }
+    case STRING :
+      if (b->kind != STRING){
+          printf("Invalid operation\n");
+          return NULL;
+      }
+
+      size_t length = strlen(a->data.v_string) + strlen(b->data.v_string) + 1; // +1 for \0
+      char *new_string = (char *) malloc(sizeof(char) * length);
+      strcat(new_string, a->data.v_string);
+      strcat(new_string, b->data.v_string);
+      // not returning just new string beacause we need free previous alloc string
+      snake_obj_t *str_obj = new_snake_string(new_string);
+      free(new_string);
+      return str_obj;
+
+    case VECTOR:
+      if (b->kind != VECTOR){
+          printf("Invalid operation\n");
+          return NULL;
+      }
+      return new_snake_vector(
+        snake_add(a->data.v_vector.x, b->data.v_vector.x),
+        snake_add(a->data.v_vector.y, b->data.v_vector.y),
+        snake_add(a->data.v_vector.z, b->data.v_vector.z)
+        );
+    case ARRAY:
+      if (b->kind != ARRAY){
+          printf("Invalid operation\n");
+          return NULL;
+      }
+      size_t arr_size = a->data.v_array.size + b->data.v_array.size;
+      snake_obj_t *arr_obj = new_snake_array(arr_size);
+
+      for(int i = 0; i < a->data.v_array.size; ++i){
+        snake_array_set(
+          arr_obj,
+          i,
+          snake_array_get(a, i)
+        );
+      }
+
+      for(int i = 0; i < a->data.v_array.size; ++i){
+        snake_array_set(
+          arr_obj,
+          i + a->data.v_array.size,
+          snake_array_get(b, i)
+        );
+      }
+      return arr_obj;
+    default :
+      printf("Invalid operation\n");
+      return NULL;
+  }
 }
